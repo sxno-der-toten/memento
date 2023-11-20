c740914047c51f9f406ce446558b34b7cf6ac4c4
sano <fawez.azzouk@coda-student.school>
2023-11-20 10:21:35 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" type="text/css" href="index.css">
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Memento</title>
+</head>
+<header>Memento
+  <ul class="head">
+  <li class="log">Login</li>
+  <li class="reg">Register</li>
+  </ul>
+<body>
+
+</body>
+</html>
